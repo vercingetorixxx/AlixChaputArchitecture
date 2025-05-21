36714480eb6d8995667eb1a13f1d1f4929c0682c
Chanwu Oh <ohchanwu@pm.me>
2025-05-21 17:12:46 +0900

    darkmode/lightmode functionality update

--- fr/script.js
+++ fr/script.js
@@ -17,154 +17,161 @@ window.addEventListener('scroll', handleScrolldown);
 handleScrolldown();
 
 
-const toggleDarkButton = document.getElementById('dark-mode-toggle');
 
-toggleDarkButton.addEventListener('click', () => {
-    if (document.body.classList.contains('darkmode')) {
-        document.body.classList.remove('darkmode');
-        document.body.classList.add('lightmode');
-    } else if (document.body.classList.contains('lightmode')) {
-        document.body.classList.remove('lightmode');
-        document.body.classList.add('darkmode');
-    } else {
-        document.body.classList.add('darkmode'); // Default to lightmode
-    }
+// -------- DARKMODE - LIGHTMODE TOGGLE -----------
 
-    const isDarkMode = document.body.classList.contains('darkmode');
-    const isLightMode = document.body.classList.contains('lightmode');
+const toggleDarkButtons = document.querySelectorAll('.dark-mode-toggle');
 
-    if (isDarkMode) {
-        document.documentElement.style.setProperty(
-            '--bg_gradient_colors', 
-            `rgb(100, 90, 110) 0%,
-            rgb(80, 100, 120) 20%,
-            rgb(120, 120, 100) 50%,
-            rgb(140, 90, 110) 80%,
-            rgb(100, 90, 110) 100%`
-        );
-        document.documentElement.style.setProperty(
-            '--container_background',
-            'rgba(0,0,0,0.5)'
-        );
-    } 
-    else {
-        document.documentElement.style.setProperty(
-            '--bg_gradient_colors', 
-            `rgb(225, 216, 230) 0%,
-            rgb(210, 230, 240) 20%,
-            rgb(240, 240, 220) 50%,
-            rgb(255, 220, 230) 80%,
-            rgb(225, 216, 230) 100%`
-        );
-        document.documentElement.style.setProperty(
-            '--container_background',
-            'rgba(255,255,255,0.5)'
-        );
-    }
+toggleDarkButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (document.body.classList.contains('darkmode')) {
+            document.body.classList.remove('darkmode');
+            document.body.classList.add('lightmode');
+        } else if (document.body.classList.contains('lightmode')) {
+            document.body.classList.remove('lightmode');
+            document.body.classList.add('darkmode');
+        } else {
+            document.body.classList.add('darkmode'); // Default to lightmode
+        }
 
-    if (isLightMode) {
-        document.documentElement.style.setProperty(
-            '--bg_gradient_colors', 
-            `rgb(225, 216, 230) 0%,
-            rgb(210, 230, 240) 20%,
-            rgb(240, 240, 220) 50%,
-            rgb(255, 220, 230) 80%,
-            rgb(225, 216, 230) 100%`
-        );
-        document.documentElement.style.setProperty(
-            '--container_background',
-            'rgba(255,255,255,0.5)'
-        );
-    }
-    else {
-        document.documentElement.style.setProperty(
-            '--bg_gradient_colors', 
-            `rgb(100, 90, 110) 0%,
-            rgb(80, 100, 120) 20%,
-            rgb(120, 120, 100) 50%,
-            rgb(140, 90, 110) 80%,
-            rgb(100, 90, 110) 100%`
-        );
-        document.documentElement.style.setProperty(
-            '--container_background',
-            'rgba(0,0,0,0.5)'
-        );
-    } 
+        const isDarkMode = document.body.classList.contains('darkmode');
+        const isLightMode = document.body.classList.contains('lightmode');
+
+        if (isDarkMode) {
+            document.documentElement.style.setProperty(
+                '--bg_gradient_colors', 
+                `rgb(100, 90, 110) 0%,
+                rgb(80, 100, 120) 20%,
+                rgb(120, 120, 100) 50%,
+                rgb(140, 90, 110) 80%,
+                rgb(100, 90, 110) 100%`
+            );
+            document.documentElement.style.setProperty(
+                '--container_background',
+                'rgba(0,0,0,0.5)'
+            );
+        } 
+        else {
+            document.documentElement.style.setProperty(
+                '--bg_gradient_colors', 
+                `rgb(225, 216, 230) 0%,
+                rgb(210, 230, 240) 20%,
+                rgb(240, 240, 220) 50%,
+                rgb(255, 220, 230) 80%,
+                rgb(225, 216, 230) 100%`
+            );
+            document.documentElement.style.setProperty(
+                '--container_background',
+                'rgba(255,255,255,0.5)'
+            );
+        }
+
+        if (isLightMode) {
+            document.documentElement.style.setProperty(
+                '--bg_gradient_colors', 
+                `rgb(225, 216, 230) 0%,
+                rgb(210, 230, 240) 20%,
+                rgb(240, 240, 220) 50%,
+                rgb(255, 220, 230) 80%,
+                rgb(225, 216, 230) 100%`
+            );
+            document.documentElement.style.setProperty(
+                '--container_background',
+                'rgba(255,255,255,0.5)'
+            );
+        }
+        else {
+            document.documentElement.style.setProperty(
+                '--bg_gradient_colors', 
+                `rgb(100, 90, 110) 0%,
+                rgb(80, 100, 120) 20%,
+                rgb(120, 120, 100) 50%,
+                rgb(140, 90, 110) 80%,
+                rgb(100, 90, 110) 100%`
+            );
+            document.documentElement.style.setProperty(
+                '--container_background',
+                'rgba(0,0,0,0.5)'
+            );
+        } 
+    });
 });
 
 
 
-const toggleLightButton = document.getElementById('light-mode-toggle');
+const toggleLightButtons = document.querySelectorAll('.light-mode-toggle');
 
-toggleLightButton.addEventListener('click', () => {
-    if (document.body.classList.contains('darkmode')) {
-        document.body.classList.remove('darkmode');
-        document.body.classList.add('lightmode');
-    } else if (document.body.classList.contains('lightmode')) {
-        document.body.classList.remove('lightmode');
-        document.body.classList.add('darkmode');
-    } else {
-        document.body.classList.add('lightmode'); // Default to lightmode
-    }
+toggleLightButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (document.body.classList.contains('darkmode')) {
+            document.body.classList.remove('darkmode');
+            document.body.classList.add('lightmode');
+        } else if (document.body.classList.contains('lightmode')) {
+            document.body.classList.remove('lightmode');
+            document.body.classList.add('darkmode');
+        } else {
+            document.body.classList.add('lightmode'); // Default to lightmode
+        }
 
-    const isDarkMode = document.body.classList.contains('darkmode');
-    const isLightMode = document.body.classList.contains('lightmode');
+        const isDarkMode = document.body.classList.contains('darkmode');
+        const isLightMode = document.body.classList.contains('lightmode');
 
-    if (isDarkMode) {
-        document.documentElement.style.setProperty(
-            '--bg_gradient_colors', 
-            `rgb(100, 90, 110) 0%,
-            rgb(80, 100, 120) 20%,
-            rgb(120, 120, 100) 50%,
-            rgb(140, 90, 110) 80%,
-            rgb(100, 90, 110) 100%`
-        );
-        document.documentElement.style.setProperty(
-            '--container_background',
-            'rgba(0,0,0,0.5)'
-        );
-    } 
-    else {
-        document.documentElement.style.setProperty(
-            '--bg_gradient_colors', 
-            `rgb(225, 216, 230) 0%,
-            rgb(210, 230, 240) 20%,
-            rgb(240, 240, 220) 50%,
-            rgb(255, 220, 230) 80%,
-            rgb(225, 216, 230) 100%`
-        );
-        document.documentElement.style.setProperty(
-            '--container_background',
-            'rgba(255,255,255,0.5)'
-        );
-    }
+        if (isDarkMode) {
+            document.documentElement.style.setProperty(
+                '--bg_gradient_colors', 
+                `rgb(100, 90, 110) 0%,
+                rgb(80, 100, 120) 20%,
+                rgb(120, 120, 100) 50%,
+                rgb(140, 90, 110) 80%,
+                rgb(100, 90, 110) 100%`
+            );
+            document.documentElement.style.setProperty(
+                '--container_background',
+                'rgba(0,0,0,0.5)'
+            );
+        } 
+        else {
+            document.documentElement.style.setProperty(
+                '--bg_gradient_colors', 
+                `rgb(225, 216, 230) 0%,
+                rgb(210, 230, 240) 20%,
+                rgb(240, 240, 220) 50%,
+                rgb(255, 220, 230) 80%,
+                rgb(225, 216, 230) 100%`
+            );
+            document.documentElement.style.setProperty(
+                '--container_background',
+                'rgba(255,255,255,0.5)'
+            );
+        }
 
-    if (isLightMode) {
-        document.documentElement.style.setProperty(
-            '--bg_gradient_colors', 
-            `rgb(225, 216, 230) 0%,
-            rgb(210, 230, 240) 20%,
-            rgb(240, 240, 220) 50%,
-            rgb(255, 220, 230) 80%,
-            rgb(225, 216, 230) 100%`
-        );
-        document.documentElement.style.setProperty(
-            '--container_background',
-            'rgba(255,255,255,0.5)'
-        );
-    }
-    else {
-        document.documentElement.style.setProperty(
-            '--bg_gradient_colors', 
-            `rgb(100, 90, 110) 0%,
-            rgb(80, 100, 120) 20%,
-            rgb(120, 120, 100) 50%,
-            rgb(140, 90, 110) 80%,
-            rgb(100, 90, 110) 100%`
-        );
-        document.documentElement.style.setProperty(
-            '--container_background',
-            'rgba(0,0,0,0.5)'
-        );
-    } 
+        if (isLightMode) {
+            document.documentElement.style.setProperty(
+                '--bg_gradient_colors', 
+                `rgb(225, 216, 230) 0%,
+                rgb(210, 230, 240) 20%,
+                rgb(240, 240, 220) 50%,
+                rgb(255, 220, 230) 80%,
+                rgb(225, 216, 230) 100%`
+            );
+            document.documentElement.style.setProperty(
+                '--container_background',
+                'rgba(255,255,255,0.5)'
+            );
+        }
+        else {
+            document.documentElement.style.setProperty(
+                '--bg_gradient_colors', 
+                `rgb(100, 90, 110) 0%,
+                rgb(80, 100, 120) 20%,
+                rgb(120, 120, 100) 50%,
+                rgb(140, 90, 110) 80%,
+                rgb(100, 90, 110) 100%`
+            );
+            document.documentElement.style.setProperty(
+                '--container_background',
+                'rgba(0,0,0,0.5)'
+            );
+        } 
+    });
 });
