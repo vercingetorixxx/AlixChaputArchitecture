f430e84eea272e5e69d99ce37379adb0eb4da6af
Chanwu Oh <ohchanwu@pm.me>
2025-05-24 12:03:41 +0900

    bulkedit

---
+++ fr/about.js
@@ -0,0 +1,48 @@
+// -------- .si-container flip ----------- //
+
+
+document.querySelectorAll('.si-container').forEach(container => {
+    container.addEventListener('click', () => {
+        container.querySelector('.flipper').classList.toggle('flipped');
+        container.classList.add('flipping');
+
+        // Remove flipping class after transition ends
+        setTimeout(() => {
+        container.classList.remove('flipping');
+        }, 100);
+    });
+});
+
+
+// -------- image slideshow ----------- //
+
+
+const container = document.querySelector('.coverimage');
+const slides = document.querySelector('.slides');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+const images = slides.querySelectorAll('img');
+let imageWidth = container.clientWidth;
+let currentIndex = 0;
+
+function updateTransform() {
+    slides.style.transform = `translateX(-${currentIndex * imageWidth}px)`;
+}
+
+// Set initial position
+updateTransform();
+
+next.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateTransform();
+});
+
+prev.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateTransform();
+});
+
+window.addEventListener('resize', () => {
+    imageWidth = container.clientWidth;
+    updateTransform();
+});
---
+++ fr/contact.js
@@ -0,0 +1,36 @@
+// -------- Email Dialog ----------- //
+
+
+const dialogs = document.querySelectorAll('dialog');
+const mainContent = document.querySelector('main');
+const successDialog = document.getElementById('successDialog');
+const failDialog = document.getElementById('failDialog');
+
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    fetch(this.action, {
+        method: 'POST',
+        body: new FormData(this),
+        headers: { 'Accept': 'application/json' }
+    })
+    .then(response => {
+        if (response.ok) {
+            mainContent.setAttribute("inert", "");
+            successDialog.showModal();
+            successDialog.style.display = "flex";
+        }
+        else {
+            mainContent.setAttribute("inert", "");
+            failDialog.showModal();
+            failDialog.style.display = "flex"; 
+        }
+    });
+});
+
+dialogs.forEach(dialog => {
+    dialog.addEventListener("click", () => {
+        dialog.close();
+        dialog.style.display = "none";
+        mainContent.removeAttribute("inert");
+    });
+})
